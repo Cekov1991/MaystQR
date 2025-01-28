b1abcd9e65f5a9d0026f0202e81465ea4c06e55d
Stefan <stefan@hubbyesim.com>
2025-01-28 12:15:52 +0100

    added subscription plans table

--- database/migrations/2025_01_28_124130_create_subscriptions_table.php
+++ database/migrations/2025_01_28_124130_create_subscriptions_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('subscription_plan_id')->constrained();
             $table->string('status')->default('active');
-            $table->integer('dynamic_qr_limit')->default(5);
-            $table->integer('scans_per_code')->default(1000);
-            $table->float('current_price')->default(5.00);
+            $table->integer('dynamic_qr_limit')->default(5); // Copied from plan at subscription time
+            $table->integer('scans_per_code')->default(1000); // Copied from plan at subscription time
+            $table->float('current_price')->default(5.00); // Keeps track of the actual price paid
             $table->timestamp('next_billing_date');
             $table->timestamps();
         });
