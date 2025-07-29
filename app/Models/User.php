a2560259d7facbf665a36e7090ede8d2f70327f3
Stefan <stefan@hubbyesim.com>
2025-07-29 11:01:19 +0200

    implements FilamentUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
