d86f8cd9944ca1ee6285bb74c64f76ec2414f870
Stefan <stefan@hubbyesim.com>
2025-08-05 14:47:41 +0200

    New prices in seeder

--- database/seeders/QrCodePackageSeeder.php
+++ database/seeders/QrCodePackageSeeder.php
@@ -12,28 +12,28 @@ class QrCodePackageSeeder extends Seeder
         QrCodePackage::create([
             'name' => '1 Month',
             'duration_months' => 1,
-            'price' => 2.99,
+            'price' => 4.99,
             'is_active' => true,
         ]);
 
         QrCodePackage::create([
             'name' => '3 Months',
             'duration_months' => 3,
-            'price' => 4.99,
+            'price' => 12.99,
             'is_active' => true,
         ]);
 
         QrCodePackage::create([
             'name' => '6 Months',
             'duration_months' => 6,
-            'price' => 9.99,
+            'price' => 22.99,
             'is_active' => true,
         ]);
 
         QrCodePackage::create([
             'name' => '12 Months',
             'duration_months' => 12,
-            'price' => 17.99,
+            'price' => 34.99,
             'is_active' => true,
         ]);
     }
